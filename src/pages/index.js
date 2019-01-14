6f439ce70396ce83d23f89dec891da3d29ad8af8
Yash Thakkar <thakkaryash94@gmail.com>
2019-01-15 04:13:48 +0530

    add dropzone support for existing docker-compose file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
+import React, { useState } from 'react'
+import { safeLoad } from 'js-yaml'
+import { DropZone } from '@shopify/polaris'
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
-
 import ServiceForm from '../components/ServiceForm'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`docker`, `compose`, `yaml`]} />
-    <ServiceForm />
-  </Layout>
-)
+export default () => {
+  const [initialState, setInitialState] = useState({})
 
-export default IndexPage
+  function setDropped(acceptedFiles) {
+    // Do something with files
+    var file = acceptedFiles[0]
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const yamlData = event.target.result
+      const jsonData = safeLoad(yamlData, 'utf8')
+      setInitialState(() => jsonData)
+    };
+    reader.readAsText(file)
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`docker`, `compose`, `yaml`]} />
+      {/* <DropZone
+        onDrop={(files, acceptedFiles, rejectedFiles) => {
+          setDropped(acceptedFiles)
+        }}
+      ></DropZone> */}
+      <ServiceForm initialState={initialState || undefined} />
+    </Layout>
+  )
+}
